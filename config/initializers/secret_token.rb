a0b331228a8399b8dbfed62f8d20143809f8d9d1
Sean O'Hara <sohara@sohara.com>
2010-09-16 18:53:42 -0400

    initial upgrade to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qwf::Application.config.secret_token = 'dc5f4ab2fb61854a173eb0751ca3fe0335af95f9208323a996895f781a3c1b25127685d46c9fe32eca4cb70b655b32911045b0a7f47cff93adf170ea6dde6890'
