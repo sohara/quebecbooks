5eb3d0b5bef52199ebad2f67627e82bf09f83180
Sean O'Hara <sohara@sohara.com>
2018-08-09 10:02:53 -0400

    Sort authorships by date_published desc

--- app/models/author.rb
+++ app/models/author.rb
@@ -20,24 +20,31 @@
 #
 
 class Author < ActiveRecord::Base
-  
+
   has_many :authorships
-  has_many :books, :through => :authorships
-  has_many :translations, :through => :authorships, :source => :book, :conditions => "authorships.kind='Translator'"
-  has_many :writings, :through => :authorships, :source => :book, :conditions => "authorships.kind='Author'"
+  has_many :books, :through => :authorships,
+    :order => 'date_published desc'
+  has_many :translations, :through => :authorships,
+    :source => :book,
+    :conditions => "authorships.kind='Translator'",
+    :order => 'date_published desc'
+  has_many :writings, :through => :authorships,
+    :source => :book,
+    :conditions => "authorships.kind='Author'",
+    :order => 'date_published desc'
   has_many :images
   validates_presence_of :first_name, :last_name
-  
+
   # composed_of declaration maps Author attributes to the Name model object
   composed_of :name,
               :class_name => "Name",
-              :mapping => 
+              :mapping =>
                 [ [ :first_name, :first ],
                   [ :other_name, :other],
                   [ :last_name, :last]
                 ]
-                
+
   cattr_reader :per_page
   @@per_page = 20
-  
+
 end
